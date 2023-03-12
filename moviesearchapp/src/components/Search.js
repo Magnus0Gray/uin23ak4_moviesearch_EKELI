56adc54c0f2bb55942107b5bcf32df85c18e3d6f
Magnus0Gray <111729773+Magnus0Gray@users.noreply.github.com>
2023-03-13 00:51:32 +0100

    Added error handling for 3 letters or less

--- moviesearchapp/src/components/Search.js
+++ moviesearchapp/src/components/Search.js
@@ -1,18 +1,37 @@
+import { useState } from 'react'
+
 export default function Search({ setSearch, getMovies }) {
+    const [inputvalue, setInputvalue] = useState('')
+    const [isValid, setIsValid] = useState(true)
 
     const handleSearch = (event) => {
-        //console.log(event.target.value)
-        setSearch(event.target.value);
+        console.log(event.target.value)
+        setInputvalue(event.target.value)
+         
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
-    }
 
-    return (
+    }
+    const handleClick = (event) => {
+        if (inputvalue.length >= 4) {
+            setSearch(inputvalue)
+            getMovies()
+            setIsValid(true)
+        }
+        else
+            setIsValid(false)
+	}
+
+
+
+    return (<>
+        {isValid === false ? <p className="error-text">Please use 4 letters or more.</p> : null}
         <form onSubmit={handleSubmit}>
-            <input type="search" placeholder="James Bond..." onChange={handleSearch} />
-            <button type="submit" onClick={getMovies}>Search</button>
+            <input type="search" name="search" placeholder="James Bond..." onChange={handleSearch} />
+            <button type="submit" onClick={handleClick}>Search</button>
         </form>
+        </>
     )
 }
